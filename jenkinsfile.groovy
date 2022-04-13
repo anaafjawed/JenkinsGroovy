8ce02ef6139ccba48b21f165ddf95c96c9ecfe42
aliamjad94 <100680915+aliamjad94@users.noreply.github.com>
2022-04-13 05:40:15 -0700

    Update jenkinsfile.groovy
    Should work now

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,17 +1,15 @@
 node{
-        stages{
-            stage('SCM'){
+     stage('SCM'){
                 steps {
                     git branch: 'main', url: 'https://github.com/aliamjad94/TradeApp.git'
                 }
             }
             
-            stage('Build'){
+     stage('Build'){
                 steps {
                     bat "\"${tool 'MSBuildLocal'}\" TradeNowApp/TradeNow.sln /p:Configuration=Debug /p:ProductVersion=1.3.0.${env.BUILD_NUMBER}"
                 }
             }
-        }
         
         post{
             always{
