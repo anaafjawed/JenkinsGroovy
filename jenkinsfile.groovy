8459fdeb1d49551b54b959efd6d34aeabdd422a5
aliamjad94 <100680915+aliamjad94@users.noreply.github.com>
2022-04-13 05:20:50 -0700

    Update jenkinsfile.groovy
    Changes to jenkinsfile

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,11 +1,25 @@
-node {
-    stage('Build') {
-        echo 'Building....'
+pipeline{
+
+    agent any
+        stages{
+            stage('Checkout Code'){
+                steps {
+                    git branch: 'main', url: 'https://github.com/aliamjad94/TradeApp.git'
+                }
+            }
+            
+            stage('Build'){
+                steps {
+                    bat "\"${tool 'MSBuildLocal'}\" TradeNowApp/TradeNow.sln /p:Configuration=Debug /p:ProductVersion=1.3.0.${env.BUILD_NUMBER}"
+                }
+            }
+        }
+        
+        post{
+            always{
+                mail to: 'smaaz1994@gmail.com',
+                subject: "Job '${JOB_NAME}' (${BUILD_NUMBER}) is ${currentBuild.result}",
+                body: "Please go to ${BUILD_URL} and verify the build!\n\nRegards,\nAmjad"
+            }
+        }
     }
-    stage('Test') {
-        echo 'Testing....'
-    }
-    stage('Deploy') {
-        echo 'Deploying....'
-    }
-}
